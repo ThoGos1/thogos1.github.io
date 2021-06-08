be314344405abec82d3b32d2fa9af8822f62282d
ThoGos <34140015+ThoGos1@users.noreply.github.com>
2021-06-07 18:36:35 -0700

    Added Tensorflow to the subpage

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-// JavaScript
+const webcamElement = document.getElementById('webcam');
 
-var arr = [1, 2];
-var cool = 2;
+let net;
 
-function changeVar()
-{
-    cool++;
-    console.log("cool value: " + cool);
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Successfully loaded model');
+
+  // Create an object from Tensorflow.js data API which could capture image
+  // from the web camera as Tensor.
+  const webcam = await tf.data.webcam(webcamElement);
+  while (true) {
+    const img = await webcam.capture();
+    const result = await net.classify(img);
+
+    document.getElementById('console').innerText = `
+      Prediction: ${result[0].className}\n
+      Probability: ${result[0].probability}
+    `;
+    // Dispose the tensor to release the memory.
+    img.dispose();
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
 }
 
-
-function showAlert()
-{
-    alert("Here it is");
-}
+app();
