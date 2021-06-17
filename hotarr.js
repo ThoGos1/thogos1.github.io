390dbf24e56ef89386a8e35981dcd484fcab20ab
ThoGos <34140015+ThoGos1@users.noreply.github.com>
2021-06-16 21:38:21 -0700

    Added floor plan

--- hotarr.js
+++ hotarr.js
@@ -1,5 +1,9 @@
 // Start of Hotel JavaScript file
 var arr = Array.from(Array(3), () => new Array(3))
+var str = "";
+var str1 = "";
+var str2 = "";
+
 
 for(var f = 0; f < arr.length; f++)
 {
@@ -9,26 +13,48 @@ for(var f = 0; f < arr.length; f++)
     }
 }
 
+
 arr[0][0] = 'Thomas';
+arr[1][1] = 'Mette';
+arr[2][2] = 'Luggi';
 
 
-
-
-
-
-
-// Prints out the 2D Array
-for(var i = 0; i < arr.length; i++)
+for(var j = 0; j < arr.length; j++)
 {
-    for(var j = 0; j < arr[i].length; j++)
-    {
-        console.log(arr[i][j]);
-    }
+    str += arr[0][j];
+    str += " ";
 }
 
+for(var k = 0; k < arr.length; k++)
+{
+    str1 += arr[1][k];
+    str1 += " ";
+}
+
+for(var h = 0; h < arr.length; h++)
+{
+    str2 += arr[2][h];
+    str2 += " ";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("str value: " + str)
+console.log('I am \nbruh')
+
+// Prints out the arr as a table in the console
 console.table(arr)
-
-
-
-
-
+// End of JS File
