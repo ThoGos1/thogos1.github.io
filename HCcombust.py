8c33623dc075a02cc345572e8edb318117a59bf0
ThoGos <34140015+ThoGos1@users.noreply.github.com>
2022-02-22 23:53:13 -0800

    Fully integrates Python so it works.
    Next step is to fix the projects page, as now you know how to use pyodide, which will be useful for the big website.

---
+++ HCcombust.py
@@ -0,0 +1,6 @@
+import js
+# It says it doesn't work, BUT it does.
+
+hmol = inh/18.04
+
+js.hmolv = hmol
---
+++ garbo/index.js
@@ -0,0 +1,12 @@
+var ni;
+
+function f(val) {
+
+    ni = val;
+
+}
+
+
+function vf() {
+    console.log(ni);
+}
